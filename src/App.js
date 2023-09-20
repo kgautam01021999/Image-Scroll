f5bd04433ccd000ab75e081163b4c5be1d606d2e
kgautam01021999 <114284622+kgautam01021999@users.noreply.github.com>
2023-09-20 09:28:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Horizontol Scrolller</h1>
+      <div className="scroller">
+     <img src="https://images.unsplash.com/photo-1681912609128-3a05a7bdce64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"/>
+     <img src="https://images.pexels.com/photos/1583582/pexels-photo-1583582.jpeg?auto=compress&cs=tinysrgb&w=600"/>
+     <img src="https://images.unsplash.com/photo-1682344382195-553372019072?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"/>
+     <img src="https://images.unsplash.com/photo-1681912609128-3a05a7bdce64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"/>
+     <img src="https://images.unsplash.com/photo-1681912609128-3a05a7bdce64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"/>
+     <img src="https://images.unsplash.com/photo-1682247902106-2ebf112cc46c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"/>
+      
+        </div>
+      
+      
+    </div>
+  );
+}
